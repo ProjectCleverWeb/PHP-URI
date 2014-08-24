bb494874e895018d3000c8a1a330071772a209b9
Nicholas Jordon <projectcleverweb@gmail.com>
2014-08-24 12:11:53 -0400

    added project doc to top of /src files

--- src/parser.php
+++ src/parser.php
@@ -1,4 +1,20 @@
 <?php
+/**
+ * PHP URI Library
+ * 
+ * A PHP library for working with URI's, that is designed around the URI
+ * standard. Requires PHP 5.4 or later. This library replaces and extends all
+ * of PHP's parse_url() features, and even has some handy aliases.
+ * 
+ * Originally inspired by P Guardiario's work.
+ * 
+ * @author    Nicholas Jordon
+ * @link      https://github.com/ProjectCleverWeb/PHP-URI
+ * @copyright 2014 Nicholas Jordon - All Rights Reserved
+ * @version   2.0.0
+ * @license   http://opensource.org/licenses/MIT
+ * @see       http://en.wikipedia.org/wiki/URI_scheme
+ */
 
 namespace uri;
 
