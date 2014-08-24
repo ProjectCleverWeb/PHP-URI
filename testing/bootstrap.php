5b82f670f7ac8cf06ced7267aa3f62cb81a54317
Nicholas Jordon <projectcleverweb@gmail.com>
2014-08-24 12:08:14 -0400

    added testing bootstrap file

---
+++ testing/bootstrap.php
@@ -0,0 +1,21 @@
+<?php
+
+
+spl_autoload_register(function ($class) {
+	$prefix = 'uri';
+	$base_dir = realpath(__DIR__.'/../src');
+	
+	$prefix_len = strlen($prefix);
+	if(strncmp($prefix, $class, $prefix_len) !== 0) {
+		return;
+	}
+	
+	$file = $base_dir.str_replace('\\', DIRECTORY_SEPARATOR, substr($class, $prefix_len)).'.php';
+	
+	if(file_exists($file) && is_file($file)) {
+		require_once $file;
+	}
+});
+
+
+class uri extends \uri\main {}
